9b001c8b26555032b5c28bdcca551f12cf553cc3
Kaitlyn Robey <katie.elena2000@gmail.com>
2017-09-13 19:22:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan") {
+  console.log("Name is correct")
+} else {
+  console.log("Name isn't right.")
+}
+
 
 // Don't worry about this
 module.exports = { name, height
